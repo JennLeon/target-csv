3a6708cd2d78c89c3a6f768aeaae837aefe575c7
jennleon <flink2020>
2022-07-20 17:01:45 +0200

    restore file, removing the date for the file name

--- target_csv.py
+++ target_csv.py
@@ -39,7 +39,7 @@ def flatten(d, parent_key='', sep='__'):
     return dict(items)
 
 
-def persist_messages(filename, delimiter, quotechar, messages, destination_path, fixed_headers, validate):
+def persist_messages(delimiter, quotechar, messages, destination_path, fixed_headers, validate):
     state = None
     schemas = {}
     key_properties = {}
@@ -62,7 +62,7 @@ def persist_messages(filename, delimiter, quotechar, messages, destination_path,
             if validate:
                 validators[o['stream']].validate(o['record'])
 
-            filename = filename + '.csv' if filename else o['stream'] + '-' + now + '.csv'
+            filename = o['stream'] + '.csv'
             filename = os.path.expanduser(os.path.join(destination_path, filename))
             file_is_empty = (not os.path.isfile(filename)) or os.stat(filename).st_size == 0
 
@@ -107,7 +107,7 @@ def persist_messages(filename, delimiter, quotechar, messages, destination_path,
             key_properties[stream] = o['key_properties']
         else:
             logger.warning("Unknown message type {} in message {}"
-                           .format(o['type'], o))
+                            .format(o['type'], o))
 
     return state
 
@@ -149,13 +149,12 @@ def main():
         threading.Thread(target=send_usage_stats).start()
 
     input_messages = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
-    state = persist_messages(filename=config.get('filename', ''),
-                             delimiter=config.get('delimiter', '\t'),
-                             quotechar=config.get('quotechar', '"'),
-                             messages=input_messages,
-                             destination_path=config.get('destination_path', ''),
-                             fixed_headers=config.get('fixed_headers'),
-                             validate=config.get('validate', True))
+    state = persist_messages(config.get('delimiter', ','),
+                             config.get('quotechar', '"'),
+                             input_messages,
+                             config.get('destination_path', ''),
+                             config.get('fixed_headers'),
+                             config.get('validate', True))
 
     emit_state(state)
     logger.debug("Exiting normally")
